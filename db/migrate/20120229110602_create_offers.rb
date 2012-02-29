362e830e39606304992afbea9cd56761e7c26f5e
Viljar K <viljark@gmail.com>
2012-02-29 13:43:04 +0200

    Updated models

--- db/migrate/20120229110602_create_offers.rb
+++ db/migrate/20120229110602_create_offers.rb
@@ -1,11 +1,12 @@
 class CreateOffers < ActiveRecord::Migration
   def change
     create_table :offers do |t|
+      t.integer :provider_id, :null => false
       t.string :name, :limit => 255, :null => false
       t.text :description
-      t.integer :count
+      t.integer :count, :default => 0, :null => false
       t.decimal :price, :scale => 6, :precision => 2
-      t.date :enddate 
+      t.date :end_date
       t.timestamps
     end
   end
