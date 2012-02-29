be10af5c01a5da282ea79910ecef739bace78251
Viljar K <viljark@gmail.com>
2012-02-29 12:08:17 +0200

    Initial empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vilrol::Application.config.secret_token = '23f9ee9647a308be366c51bcada37ec387f0a69d68662aebd2309c33d58b99b207e7a75e087b4e48e71367574d20e7640deb738ff95657ff274596a370713753'
