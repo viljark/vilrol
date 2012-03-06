89b31fe2f4e668a17843e53bc2ad401710bd4e2f
Viljar K <viljark@gmail.com>
2012-03-06 23:31:56 +0200

    Additional models

---
+++ db/migrate/20120306204812_create_purchases.rb
@@ -0,0 +1,9 @@
+class CreatePurchases < ActiveRecord::Migration
+  def change
+    create_table :purchases do |t|
+      t.integer :user_id, :null => false
+      t.integer :offer_id, :null => false
+      t.timestamps
+    end
+  end
+end
