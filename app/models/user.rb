cc190eec4f26396c4ef257e42a6a33c5ad3e9ac4
Viljar K <viljark@gmail.com>
2012-03-14 13:30:53 +0200

    Scaffold for Users and Offers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  validates :username, :hashed_password, :email, :presence => true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Vilrol::Application.routes.draw do
+  resources :offers
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
